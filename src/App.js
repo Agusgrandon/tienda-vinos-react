e7a6723892583356eae06771c886da87cefebcff
agustina grandon <agustina107.grandon@gmail.com>
2023-04-14 23:31:52 -0300

    agrege footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
       <>
       <NavBar/>
       <ItemListContainer greeting={"Todos los productos"}/>
+      <Footer/>
       </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Footer.css';
+
+export default function Footer() {
+    return <>
+        <div className="fondo container">
+            <div className="row justify-content-between align-items-center row-contenedor">
+                <a href="#" className="col-md-12 text-center">Instagram</a>
+                <a href="#" className="col-md-12 text-center">Facebook</a>
+                <a href="#" className="col-md-12 text-center">Preguntas Frecuentes</a>
+                <p className="col-md-12 text-center">@copyright2022</p>
+
+            </div>
+        </div>
+        </>
+    
+}

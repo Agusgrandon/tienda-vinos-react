01416ca50baa4042a91907f026978c72b57fe735
agustina grandon <agustina107.grandon@gmail.com>
2023-04-14 23:58:19 -0300

    cambie estilos

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -19,13 +19,13 @@ useEffect(() =>{
 
     return (
         <div className="counter">
-            <button disabled={count <= 1} onClick={decrease}>-</button>
-            <span>{count}</span>
-            <button disabled={count >= stock} onClick={increase}>+</button>
-            <div>
-            <button disabled={stock <= 0} onClick={() => onAdd(count)}>Agregar al carrito</button>
-            </div>
+        <button disabled={count <= 1} onClick={decrease}>-</button>
+        <span>{count}</span>
+        <button disabled={count >= stock} onClick={increase}>+</button>
+        <div>
+        <button disabled={stock <= 0} onClick={() => onAdd(count)}>Agregar al carrito</button>
         </div>
+    </div>
     )
 
 }

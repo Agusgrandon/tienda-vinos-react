c3d8217ccef2687679f9b387503bd9efb78a69e2
agustina grandon <agustina107.grandon@gmail.com>
2023-04-17 19:03:01 -0300

    agrege el navegador

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -5,15 +5,9 @@ import {useParams} from 'react-router-dom';
 
 
 const vinos = [
-    { id: 1, category: "rosado", title: "Vino Rosado", price: "1000", image: "https://labebidadetusfiestas.com.ar/45496-thickbox_default/dilema-pink-750cc.jpg" },
-    { id: 2, category: "blanco", title: "Vino Blanco", price: "1000", image: "https://cdn.shopify.com/s/files/1/0005/4634/0925/products/Marlo-Nueva-1000x1000_2048x2048.jpg?v=1639399770" },
-    { id: 3, category: "tinto", title: "Vino Tinto", price: "1000", image: "https://arjosimarprod.vteximg.com.br/arquivos/ids/158641-1000-1000/Vino-Cabernet-Sauvignon-Norton-1895-750-cc-1-4899.jpg?v=637413265986470000" },
-    { id: 4, category: "rosado", title: "Vino Rosado", price: "1000", image: "https://labebidadetusfiestas.com.ar/45496-thickbox_default/dilema-pink-750cc.jpg" },
-    { id: 5, category: "blanco", title: "Vino Blanco", price: "1000", image: "https://cdn.shopify.com/s/files/1/0005/4634/0925/products/Marlo-Nueva-1000x1000_2048x2048.jpg?v=1639399770" },
-    { id: 6, category: "tinto", title: "Vino Tinto", price: "1000", image: "https://arjosimarprod.vteximg.com.br/arquivos/ids/158641-1000-1000/Vino-Cabernet-Sauvignon-Norton-1895-750-cc-1-4899.jpg?v=637413265986470000" },
-    { id: 7, category: "blanco", title: "Vino Blanco", price: "1000", image: "https://cdn.shopify.com/s/files/1/0005/4634/0925/products/Marlo-Nueva-1000x1000_2048x2048.jpg?v=1639399770" },
-    { id: 8, category: "tinto", title: "Vino Tinto", price: "1000", image: "https://arjosimarprod.vteximg.com.br/arquivos/ids/158641-1000-1000/Vino-Cabernet-Sauvignon-Norton-1895-750-cc-1-4899.jpg?v=637413265986470000" },
-    
+    { id: "1", category: "rosado", title: "Vino Rosado", price: "1000", image: "https://labebidadetusfiestas.com.ar/45496-thickbox_default/dilema-pink-750cc.jpg" },
+    { id: "2", category: "blanco", title: "Vino Blanco", price: "1000", image: "https://cdn.shopify.com/s/files/1/0005/4634/0925/products/Marlo-Nueva-1000x1000_2048x2048.jpg?v=1639399770" },
+    { id: "3", category: "tinto", title: "Vino Tinto", price: "1000", image: "https://arjosimarprod.vteximg.com.br/arquivos/ids/158641-1000-1000/Vino-Cabernet-Sauvignon-Norton-1895-750-cc-1-4899.jpg?v=637413265986470000" },
 ];
 
 

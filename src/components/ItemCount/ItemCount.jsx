e4de7ceb25ca61e96c81b203a8bba1d7e9839375
agustina grandon <agustina107.grandon@gmail.com>
2023-04-17 18:55:28 -0300

    agrege fotos

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -19,11 +19,11 @@ useEffect(() =>{
 
     return (
         <div className="counter">
-        <button type="button" class="btn btn-dark btn-sm btn-block" disabled={count <= 1} onClick={decrease}>-</button>
+        <button type="button" class="btn btn-outline-dark btn-sm btn-block" disabled={count <= 1} onClick={decrease}>-</button>
         <span>{count}</span>
-        <button type="button" class="btn btn-dark btn-sm btn-block" disabled={count >= stock} onClick={increase}>+</button>
+        <button type="button" class="btn btn-outline-dark btn-sm btn-block" disabled={count >= stock} onClick={increase}>+</button>
         <div>
-        <button type="button" class="btn btn-dark btn-sm" disabled={stock <= 0} onClick={() => onAdd(count)}>Agregar al carrito</button>
+        <button type="button" class="btn btn-outline-dark btn-sm" disabled={stock <= 0} onClick={() => onAdd(count)}>Agregar al carrito</button>
         </div>
     </div>
     )

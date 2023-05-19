6fc3bcaaf8261e876ea50ec34cb9eb5e06ccd084
agustina grandon <agustina107.grandon@gmail.com>
2023-05-19 19:32:04 -0300

    cambie el detalle

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,28 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import ItemDetail from "../ItemDetail/ItemDetail";
 import { useParams } from 'react-router-dom';
 
-const vinos = [
-    { id: "1", category: "rosado", title: "Vino Rosado", price: 1000, image: "https://labebidadetusfiestas.com.ar/45496-thickbox_default/dilema-pink-750cc.jpg" },
-    { id: "2", category: "blanco", title: "Vino Blanco", price: 1000, image: "https://cdn.shopify.com/s/files/1/0005/4634/0925/products/Marlo-Nueva-1000x1000_2048x2048.jpg?v=1639399770" },
-    { id: "3", category: "tinto", title: "Vino Tinto", price: 1000, image: "https://arjosimarprod.vteximg.com.br/arquivos/ids/158641-1000-1000/Vino-Cabernet-Sauvignon-Norton-1895-750-cc-1-4899.jpg?v=637413265986470000" },
-
-];
-
 export const ItemDetailContainer = () => {
     const [data, setData] = useState({});
-    //filtro
     const { detalleId } = useParams();
-    //promesa
 
     useEffect(() => {
-        const getData = new Promise(resolve => {
-            setTimeout(() => {
-                resolve(vinos);
-            }, 1000);
-        });
-        //filtro
-        getData.then(res => setData(res.find(vino => vino.id === detalleId)));
+        const querydb = getFirestore();
+        const queryDoc = doc(querydb, 'products', 'SDMk1pS3CYxLRu0g7WTC');
+        getDoc(queryDoc)
+            .then(res => setData({ id: res.id, ...res.data() }))
     }, [])
 
     return (

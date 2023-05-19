d107621290531fae48bbad05d0756bf7bfe1fa4b
agustina grandon <agustina107.grandon@gmail.com>
2023-05-18 22:33:30 -0300

    arregle css

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -14,19 +14,20 @@ export const ItemDetail = ({ data }) => {
   addProduct(data, quantity);
 }
   return (
-    <div className="card text-bg-dark mb-3">
+    <div className="card text-bg-light mb-3">
       <div className="row g-0">
         <div className="col-md-4">
           <img src={data.image} className="card-img-top" alt={data.title} />
         </div>
         <div className="col-md-8">
           <div className="card-body">
-            <h5 className="tituloDetail">{data.title}</h5>
-            <p className="palabra">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quo error aliquid itaque ad, labore ratione voluptates? Inventore impedit perferendis quidem dolor, repudiandae alias nisi mollitia ducimus incidunt voluptate perspiciatis ex temporibus dolore vel corporis neque dolorem illum quod. Deleniti rerum nihil vitae aliquam beatae quia excepturi quam officia dolor?</p>
-            <p className="precio text-center">Precio ${data.price}</p>
+            <h5 className="detalleTitulo">{data.title}</h5>
+            <p className="detallePrecio text-center">Precio ${data.price}</p>
+            <p className="palabraDetalle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quo error aliquid itaque ad, labore ratione voluptates? Inventore impedit perferendis quidem dolor, repudiandae alias nisi mollitia ducimus incidunt voluptate perspiciatis ex temporibus dolore vel corporis neque dolorem illum quod. Deleniti rerum nihil vitae aliquam beatae quia excepturi quam officia dolor?</p>
+          
             {
               irAlCarrito
-              ? <Link className="linkCompra" to='/cart'><button type="button" className="btn btn-light">Terminar compra</button></Link>
+              ? <Link className="linkCompra" to='/cart'><button type="button" className="btn btn-dark btn-lg">Terminar compra</button></Link>
               : <ItemCount initial={1} stock={5} onAdd={onAdd} />
             }
           </div>

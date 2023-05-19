f9fb7a06828c170e5bfb71f928084e1c896920b0
agustina grandon <agustina107.grandon@gmail.com>
2023-05-19 19:07:34 -0300

    cambie firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCoSrIYd6SHZbXcQ9H3oPdiUnpxQ1ldSfw",
+  authDomain: "vinosdilema.firebaseapp.com",
+  projectId: "vinosdilema",
+  storageBucket: "vinosdilema.appspot.com",
+  messagingSenderId: "491364386213",
+  appId: "1:491364386213:web:b1c66a5a87cbbdeacf144c"
+};
+
+// Initialize Firebase
+ initializeApp(firebaseConfig);
